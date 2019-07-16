8c2d524bf8b457b4639bff974f26e6717529df41
Kacper Kowalski <kacper.kowalski@lerta.energy>
2019-07-16 15:45:21 +0200

    boundary_mapper: add unit tests

--- tests/test_boundary_mapper.cpp
+++ tests/test_boundary_mapper.cpp
@@ -11,15 +11,39 @@
 
 enum class test_enum_class
 {
-    first,
+    first = 0,
     second,
-    third
+    third,
+    fourth,
+    fifth,
+    sixth,
 };
 
-static constexpr auto boundaries{std::experimental::make_array<unsigned>(1, 2, 3, 4, 5)};
+static constexpr auto boundaries{std::experimental::make_array<unsigned>(1, 2, 3, 5, 7)};
+static constexpr auto boundaries_lowest_number{std::experimental::make_array<unsigned>(50)};
 
 TEST_CASE("boundary_mapper template class unit tests", "[boundary_mapper]")
 {
-    // static constexpr jungles::boundary_mapper<test_enum_class, decltype(boundaries.itvalue_type, 5, boundaries> bm;
-    static constexpr auto bm{jungles::make_boundary_mapper<test_enum_class, boundaries>()};
+    SECTION("Positive test for five boundaries")
+    {
+        static constexpr auto bm{jungles::make_boundary_mapper<test_enum_class, boundaries>()};
+        REQUIRE(bm.convert(0) == test_enum_class::first);
+        REQUIRE(bm.convert(1) == test_enum_class::first);
+        REQUIRE(bm.convert(2) == test_enum_class::second);
+        REQUIRE(bm.convert(3) == test_enum_class::third);
+        REQUIRE(bm.convert(4) == test_enum_class::fourth);
+        REQUIRE(bm.convert(5) == test_enum_class::fourth);
+        REQUIRE(bm.convert(6) == test_enum_class::fifth);
+        REQUIRE(bm.convert(7) == test_enum_class::fifth);
+        REQUIRE(bm.convert(8) == test_enum_class::sixth);
+        REQUIRE(bm.convert(9) == test_enum_class::sixth);
+    }
+
+    SECTION("Positive test for two boundaries (lowest possible number of boundaries)")
+    {
+        static constexpr auto bm{jungles::make_boundary_mapper<test_enum_class, boundaries_lowest_number>()};
+        REQUIRE(bm.convert(49) == test_enum_class::first);
+        REQUIRE(bm.convert(50) == test_enum_class::first);
+        REQUIRE(bm.convert(51) == test_enum_class::second);
+    }
 }
