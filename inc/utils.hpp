878b30c046e4eb6e6265bb9975decee1378ac3b7
Kacper Kowalski <kacper.kowalski@lerta.energy>
2019-07-23 12:04:24 +0200

    Rename find_nearest_boundary() to find_nearest()

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -48,10 +48,10 @@ template <typename E> constexpr auto to_u_type(E e) noexcept
 }
 
 /**
- * \brief Finds the nearest boundary, where boundaries are provided as range <it, end), for specific value val and
- *        taking into account the maximum value max_val.
+ * \brief Finds the nearest value, where the values are provided as sorted range <it, end), for specific value val and
+ *        taking into account the maximum value max_val. A
  *
- * This means that when the boundaries are e.g. 100, 200, 300 and the maximum value is 600 then:
+ * This means that when the range contains values e.g. 100, 200, 300 and the maximum value is 600 then:
  * * for val = 120 the result is 100
  * * for val = 155 the result is 200
  * * for val = 290 the result is 200
@@ -59,28 +59,28 @@ template <typename E> constexpr auto to_u_type(E e) noexcept
  * * for val = 501 the result is 100
  * * for val = 499 the result is 300
  *
- * \see The test_find_nearest_boundary.cpp which contains tests and explains in more details the idea behind that.
+ * \see The test_find_nearest.cpp which contains tests and explains in more details the idea behind that.
  */
-template <typename It, typename T> constexpr It find_nearest_boundary(It it, It end, T max_val, T val)
+template <typename It, typename T> constexpr It find_nearest(It it, It end, T max_val, T val)
 {
     if (it == end || val > max_val)
         return end;
 
-    auto highest_boundary_it{std::prev(end)};
-    auto lowest_boundary_it{it};
-    auto highest_boundary = *highest_boundary_it;
-    auto lowest_boundary = *lowest_boundary_it;
-    if (val < lowest_boundary)
+    auto highest_val_it{std::prev(end)};
+    auto lowest_val_it{it};
+    auto highest_val = *highest_val_it;
+    auto lowest_val = *lowest_val_it;
+    if (val < lowest_val)
     {
-        auto distance_to_lowest = lowest_boundary - val;
-        auto distance_to_highest = max_val - highest_boundary + val;
-        return distance_to_lowest > distance_to_highest ? highest_boundary_it : lowest_boundary_it;
+        auto distance_to_lowest = lowest_val - val;
+        auto distance_to_highest = max_val - highest_val + val;
+        return distance_to_lowest > distance_to_highest ? highest_val_it : lowest_val_it;
     }
-    else if (val > highest_boundary)
+    else if (val > highest_val)
     {
-        auto distance_to_lowest = max_val - val + lowest_boundary;
-        auto distance_to_highest = val - highest_boundary;
-        return distance_to_lowest > distance_to_highest ? highest_boundary_it : lowest_boundary_it;
+        auto distance_to_lowest = max_val - val + lowest_val;
+        auto distance_to_highest = val - highest_val;
+        return distance_to_lowest > distance_to_highest ? highest_val_it : lowest_val_it;
     }
 
     auto min{max_val};
