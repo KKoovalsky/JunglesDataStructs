acef6bd89e857222f85adcfb0fd7894f45dade9f
Kacper Kowalski <kacper.kowalski@lerta.energy>
2019-07-26 15:37:05 +0200

    binary_stream: first tests and impl attempts

--- inc/binary_stream.hpp
+++ inc/binary_stream.hpp
@@ -7,9 +7,27 @@
 #ifndef BINARY_STREAM_HPP
 #define BINARY_STREAM_HPP
 
+#include "utils.hpp"
 #include <cinttypes>
 
 namespace jungles {
+
+namespace detail {
+
+//! This is dangerous and can only be used internally unless you know what you are doing.
+template <typename T, typename It> void stream_and_advance(It &it, T val)
+{
+    auto to_ptr = &(*it);
+    auto to_byte_ptr = reinterpret_cast<uint8_t *>(to_ptr);
+
+    auto from_ptr = &val;
+    auto from_byte_ptr = reinterpret_cast<uint8_t *>(from_ptr);
+
+    std::copy(from_byte_ptr, from_byte_ptr + sizeof(T), to_byte_ptr);
+    std::advance(it, sizeof(T));
+}
+
+} // namespace detail
 
 template <std::size_t InternalBufSize> class binary_stream
 {
@@ -26,22 +44,45 @@ template <std::size_t InternalBufSize> class binary_stream
      * \returns true when all of the params were put to the stream, false if none of the params have been put.
      * \note If there is no place for all parameters to be but then no parameter is put to the stream.
      */
-    template <typename... TrivialTypes> bool write(TrivialTypes &&... params);
+    template <typename... TrivialTypes> bool write(TrivialTypes... params)
+    {
+        constexpr unsigned sizeof_params = sizeof...(params);
+        static_assert(jungles::utils::all_trivial<TrivialTypes...>::value, "The parameters must be trivial types");
+        if (space_left() < sizeof_params)
+            return false;
+
+        (jungles::detail::stream_and_advance(m_it, params), ...);
+        return true;
+    }
 
     //! Clears the stream and removes all the data from it.
-    void clear();
+    void clear()
+    {
+        m_it = std::begin(m_buf);
+    }
 
     //! Returns begin iterator to the stream. Never invalidated.
-    BinaryStreamConstIterator cbegin() const;
+    BinaryStreamConstIterator cbegin() const
+    {
+        return std::cbegin(m_buf);
+    }
 
     //! Returns end iterator to the stream. Gets invalidated after call to write() or clear().
-    BinaryStreamConstIterator cend() const;
+    BinaryStreamConstIterator cend() const
+    {
+        return static_cast<BinaryStreamConstIterator>(m_it);
+    }
 
   private:
     using BinaryStreamIterator = typename ByteArrayType::iterator;
 
     ByteArrayType m_buf;
-    BinaryStreamIterator m_it;
+    BinaryStreamIterator m_it{std::begin(m_buf)};
+
+    unsigned space_left() const
+    {
+        return std::distance(static_cast<BinaryStreamConstIterator>(m_it), std::end(m_buf));
+    }
 };
 
 } // namespace jungles
---
+++ tests/test_binary_stream.cpp
@@ -0,0 +1,77 @@
+/**
+ * @file	test_binary_stream.cpp
+ * @brief	Tests the binary_stream template class.
+ * @author	Kacper Kowalski - kacper.s.kowalski@gmail.com
+ */
+#include "ext_deps/catch/catch.hpp"
+
+#include "binary_stream.hpp"
+
+#include <cinttypes>
+
+TEST_CASE("binary_stream template class unit tests", "[binary_stream]")
+{
+    SECTION("Succesfully writes single variable when there is sufficient space")
+    {
+        jungles::binary_stream<10> bs;
+        REQUIRE(bs.write(static_cast<uint32_t>(0x00112233)));
+        const uint8_t res_be[] = {0x00, 0x11, 0x22, 0x33};
+        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(res_be)));
+    }
+
+    SECTION("Succesfully writes multiple variables when there is sufficient space")
+    {
+        jungles::binary_stream<20> bs;
+        REQUIRE(bs.write(static_cast<uint32_t>(0x44332211), static_cast<char>(0x6F), static_cast<short>(0xFFEE)));
+        const uint8_t res_be[] = {0x44, 0x33, 0x22, 0x11, 0x6F, 0xFF, 0xEE};
+        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(res_be)));
+    }
+
+    SECTION("Doesn't write single variable when there is no sufficient space")
+    {
+        jungles::binary_stream<8> bs;
+        REQUIRE(bs.write(static_cast<uint32_t>(0xAABBCCDD), static_cast<uint16_t>(0xEEFF)));
+        REQUIRE_FALSE(bs.write(static_cast<uint32_t>(0x00112233)));
+        // const uint8_t res_le[] = {0xDD, 0xCC, 0xBB, 0xAA, 0xFF, 0xEE};
+        const uint8_t res_be[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
+        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(res_be)));
+    }
+
+    SECTION("Doesn't write multiple variables when there is no sufficient space")
+    {
+        jungles::binary_stream<12> bs;
+        REQUIRE(bs.write(static_cast<uint32_t>(0xAABBCCDD), static_cast<uint16_t>(0xEEFF)));
+        REQUIRE_FALSE(bs.write(static_cast<uint32_t>(0x00112233), static_cast<uint32_t>(0x44556677)));
+        // const uint8_t res_le[] = {0xDD, 0xCC, 0xBB, 0xAA, 0xFF, 0xEE};
+        const uint8_t res_be[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
+        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(res_be)));
+    }
+
+    SECTION("Doesn't allow to write after writing to the edge of the stream")
+    {
+        SECTION("When writing single variables")
+        {
+            jungles::binary_stream<8> bs;
+            REQUIRE(bs.write(static_cast<uint32_t>(99)));
+            REQUIRE(bs.write(static_cast<uint32_t>(99)));
+            REQUIRE_FALSE(bs.write(static_cast<uint32_t>(99)));
+            REQUIRE(std::distance(bs.cbegin(), bs.cend()) == 8);
+        }
+        SECTION("When writing multiple variables")
+        {
+            jungles::binary_stream<8> bs;
+            REQUIRE(bs.write(static_cast<uint32_t>(99), static_cast<uint32_t>(99)));
+            REQUIRE_FALSE(bs.write(static_cast<uint32_t>(99)));
+            REQUIRE(std::distance(bs.cbegin(), bs.cend()) == 8);
+        }
+    }
+
+    SECTION("After clearing begin is same like end")
+    {
+        jungles::binary_stream<14> bs;
+        REQUIRE(bs.write(311));
+        REQUIRE(bs.cbegin() != bs.cend());
+        bs.clear();
+        REQUIRE(bs.cbegin() == bs.cend());
+    }
+}
