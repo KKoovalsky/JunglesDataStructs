200502611eb5a0151b602e3860184ff03a4d9b4f
Kacper <kacper.s.kowalski@gmail.com>
2019-08-05 23:01:39 +0200

    binary_stream: make tests work on little endian machine
    This commit hardcodes the tests to work on the little endian machine.
    They will fail on big endian machine.

--- tests/test_binary_stream.cpp
+++ tests/test_binary_stream.cpp
@@ -8,6 +8,10 @@
 #include "binary_stream.hpp"
 
 #include <cinttypes>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <vector>
 
 TEST_CASE("binary_stream template class unit tests", "[binary_stream]")
 {
@@ -15,16 +19,16 @@ TEST_CASE("binary_stream template class unit tests", "[binary_stream]")
     {
         jungles::binary_stream<10> bs;
         REQUIRE(bs.write(static_cast<uint32_t>(0x00112233)));
-        const uint8_t res_be[] = {0x00, 0x11, 0x22, 0x33};
-        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(res_be)));
+        const uint8_t result_little_endian[] = {0x33, 0x22, 0x11, 0x00};
+        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(result_little_endian)));
     }
 
     SECTION("Succesfully writes multiple variables when there is sufficient space")
     {
         jungles::binary_stream<20> bs;
         REQUIRE(bs.write(static_cast<uint32_t>(0x44332211), static_cast<char>(0x6F), static_cast<short>(0xFFEE)));
-        const uint8_t res_be[] = {0x44, 0x33, 0x22, 0x11, 0x6F, 0xFF, 0xEE};
-        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(res_be)));
+        const uint8_t result_little_endian[] = {0x11, 0x22, 0x33, 0x44, 0x6F, 0xEE, 0xFF};
+        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(result_little_endian)));
     }
 
     SECTION("Doesn't write single variable when there is no sufficient space")
@@ -32,9 +36,8 @@ TEST_CASE("binary_stream template class unit tests", "[binary_stream]")
         jungles::binary_stream<8> bs;
         REQUIRE(bs.write(static_cast<uint32_t>(0xAABBCCDD), static_cast<uint16_t>(0xEEFF)));
         REQUIRE_FALSE(bs.write(static_cast<uint32_t>(0x00112233)));
-        // const uint8_t res_le[] = {0xDD, 0xCC, 0xBB, 0xAA, 0xFF, 0xEE};
-        const uint8_t res_be[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
-        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(res_be)));
+        const uint8_t result_little_endian[] = {0xDD, 0xCC, 0xBB, 0xAA, 0xFF, 0xEE};
+        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(result_little_endian)));
     }
 
     SECTION("Doesn't write multiple variables when there is no sufficient space")
@@ -42,9 +45,8 @@ TEST_CASE("binary_stream template class unit tests", "[binary_stream]")
         jungles::binary_stream<12> bs;
         REQUIRE(bs.write(static_cast<uint32_t>(0xAABBCCDD), static_cast<uint16_t>(0xEEFF)));
         REQUIRE_FALSE(bs.write(static_cast<uint32_t>(0x00112233), static_cast<uint32_t>(0x44556677)));
-        // const uint8_t res_le[] = {0xDD, 0xCC, 0xBB, 0xAA, 0xFF, 0xEE};
-        const uint8_t res_be[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
-        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(res_be)));
+        const uint8_t result_little_endian[] = {0xDD, 0xCC, 0xBB, 0xAA, 0xFF, 0xEE};
+        REQUIRE(std::equal(bs.cbegin(), bs.cend(), std::begin(result_little_endian)));
     }
 
     SECTION("Doesn't allow to write after writing to the edge of the stream")
