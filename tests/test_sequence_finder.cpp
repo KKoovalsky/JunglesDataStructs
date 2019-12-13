e9d20b8432f6fa353cf736cab955c0ebe0262a13
Kacper <kacper.s.kowalski@gmail.com>
2019-12-13 22:34:04 +0100

    sequence_finder: make tests more descriptive in code
    The code of the tests is more descriptive. Catch's matchers has been
    used to obtain that. On the other hand those tests doesn't give
    descriptive output and this is a work to be done.

--- tests/test_sequence_finder.cpp
+++ tests/test_sequence_finder.cpp
@@ -8,51 +8,92 @@
 
 #include "sequence_finder.hpp"
 
+#include <algorithm>
+#include <cassert>
+#include <functional>
+#include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
+
+class sequence_finder_matcher : public Catch::MatcherBase<jungles::sequence_finder>
+{
+  public:
+    sequence_finder_matcher(std::function<bool(jungles::sequence_finder)> matcher) : m_matcher{matcher}
+    {
+    }
+
+    bool match(const jungles::sequence_finder &sf) const override
+    {
+        return m_matcher(sf);
+    }
+
+    virtual std::string describe() const override
+    {
+        std::ostringstream ss;
+        ss << "finds sequence in ";
+        return ss.str();
+    }
+
+  private:
+    std::function<bool(jungles::sequence_finder)> m_matcher;
+};
+
+struct InString
+{
+    InString(std::string s) : m_s{s}
+    {
+    }
+
+    sequence_finder_matcher FindsSequenceAtIndex(unsigned index)
+    {
+        return FindsSequenceAtIndices({index});
+    }
+
+    sequence_finder_matcher FindsSequenceAtIndices(std::vector<unsigned> indices)
+    {
+        return sequence_finder_matcher{[input_string = this->m_s, indices](jungles::sequence_finder sf) {
+            // auto make_output_message{[](bool shall_be_found, bool found, unsigned index) {
+            //     std::ostringstream ss;
+            //     ss << "At index" << index;
+            //     ss << ", sequence shall" << (shall_be_found ? " " : "not ") << "be found, ";
+            //     ss << "but" << (found ? " " : "not ") << "found";
+            //     return ss.str();
+            // }};
+
+            for (auto index : indices)
+                assert(index < input_string.size() && "Index is out-of-bound for the input string");
+            for (unsigned i = 0; i < input_string.size(); ++i)
+            {
+                auto found = sf.found(input_string[i]);
+                auto shall_be_found{std::find(std::begin(indices), std::end(indices), i) != std::end(indices)};
+                if ((shall_be_found && !found) || (!shall_be_found && found))
+                    return false;
+            }
+            return true;
+        }};
+    }
+
+    std::string m_s;
+};
 
 TEST_CASE("sequence_finder finds sequences in strings provided byte-by-byte", "[sequence_finder]")
 {
     SECTION("Single sequence at the beginning")
     {
-        jungles::sequence_finder f{"makapaka"};
-        for (auto c : std::string{"makapak"})
-        {
-            REQUIRE(!f.found(c));
-        }
-        REQUIRE(f.found('a'));
+        REQUIRE_THAT(jungles::sequence_finder{"makapaka"}, InString("makapaka").FindsSequenceAtIndex(7));
     }
     SECTION("Single sequence in the middle")
     {
-        jungles::sequence_finder f{"maka"};
-        for (auto c : std::string{"dasdasdasmak"})
-        {
-            REQUIRE(!f.found(c));
-        }
-        REQUIRE(f.found('a'));
+        REQUIRE_THAT(jungles::sequence_finder{"maka"}, InString("dasdasdasmaka").FindsSequenceAtIndex(12));
     }
     SECTION("Three sequences in a row")
     {
-        jungles::sequence_finder f{"uga"};
-        for (int i = 0; i < 3; ++i)
-        {
-            for (auto c : std::string{"ug"})
-            {
-                REQUIRE(!f.found(c));
-            }
-            REQUIRE(f.found('a'));
-        }
+        REQUIRE_THAT(jungles::sequence_finder{"uga"}, InString("ugaugauga").FindsSequenceAtIndices({2, 5, 8}));
     }
     SECTION("A sequence which contains string that is repeated in the sequence and this string is repeated 4 times so "
             "it's only found two times")
     {
-        jungles::sequence_finder f{"ugauga"};
-        for (int i = 0; i < 2; ++i)
-        {
-            for (auto c : std::string{"ugaug"})
-            {
-                REQUIRE(!f.found(c));
-            }
-            REQUIRE(f.found('a'));
-        }
+        REQUIRE_THAT(jungles::sequence_finder{"ugauga"}, InString("ugaugaugauga").FindsSequenceAtIndices({5, 11}));
     }
 }
