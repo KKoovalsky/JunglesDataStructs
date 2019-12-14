99559e967bf7efcab67111e5fe094ef6e868b930
Kacper <kacper.s.kowalski@gmail.com>
2019-12-14 11:45:22 +0100

    Rewrite sequence_finder tests to use GENERATE
    This 3rd generation of the test case uses GENERATE(), which is Catch's
    keyword used to create parametrized test cases. At this stage the DRY
    rule applies to the tests, but the tests are really hard to follow.
    Custom failure messages are applied and no REQUIRE/CHECK macros are used
    what kind'a breaks the TDD approach.
    Maybe it shall be done like that, that each test shall be unrolled, and
    each line shall contain a single:
    
    REQUIRE(sequence_finder.found('c'));
    
    ?

--- tests/test_sequence_finder.cpp
+++ tests/test_sequence_finder.cpp
@@ -9,91 +9,55 @@
 #include "sequence_finder.hpp"
 
 #include <algorithm>
-#include <cassert>
-#include <functional>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
 
-class sequence_finder_matcher : public Catch::MatcherBase<jungles::sequence_finder>
-{
-  public:
-    sequence_finder_matcher(std::function<bool(jungles::sequence_finder)> matcher) : m_matcher{matcher}
-    {
-    }
-
-    bool match(const jungles::sequence_finder &sf) const override
-    {
-        return m_matcher(sf);
-    }
-
-    virtual std::string describe() const override
-    {
-        std::ostringstream ss;
-        ss << "finds sequence in ";
-        return ss.str();
-    }
-
-  private:
-    std::function<bool(jungles::sequence_finder)> m_matcher;
-};
-
-struct InString
-{
-    InString(std::string s) : m_s{s}
-    {
-    }
-
-    sequence_finder_matcher FindsSequenceAtIndex(unsigned index)
-    {
-        return FindsSequenceAtIndices({index});
-    }
-
-    sequence_finder_matcher FindsSequenceAtIndices(std::vector<unsigned> indices)
-    {
-        return sequence_finder_matcher{[input_string = this->m_s, indices](jungles::sequence_finder sf) {
-            // auto make_output_message{[](bool shall_be_found, bool found, unsigned index) {
-            //     std::ostringstream ss;
-            //     ss << "At index" << index;
-            //     ss << ", sequence shall" << (shall_be_found ? " " : "not ") << "be found, ";
-            //     ss << "but" << (found ? " " : "not ") << "found";
-            //     return ss.str();
-            // }};
-
-            for (auto index : indices)
-                assert(index < input_string.size() && "Index is out-of-bound for the input string");
-            for (unsigned i = 0; i < input_string.size(); ++i)
-            {
-                auto found = sf.found(input_string[i]);
-                auto shall_be_found{std::find(std::begin(indices), std::end(indices), i) != std::end(indices)};
-                if ((shall_be_found && !found) || (!shall_be_found && found))
-                    return false;
-            }
-            return true;
-        }};
-    }
-
-    std::string m_s;
-};
+static std::string make_test_failure_message(bool shall_be_found, bool found, unsigned index);
 
 TEST_CASE("sequence_finder finds sequences in strings provided byte-by-byte", "[sequence_finder]")
 {
-    SECTION("Single sequence at the beginning")
+    using record = std::tuple<std::string, std::string_view, std::string, std::vector<unsigned>>;
+    auto test_parameters = GENERATE(table<std::string, std::string_view, std::string, std::vector<unsigned>>(
+        {record{"Single sequence at the beginning", "makapaka", "makapaka", {7}},
+         record{"Single sequence in the middle", "maka", "dasdasdasmaka", {12}},
+         record{"Three sequences in a row", "uga", "ugaugauga", {2, 5, 8}},
+         record{"Repeated string within the sequence", "ugauga", "ugaugaugauga", {5, 11}}}));
+
+    auto [test_description, sequence_to_find, input_string, indices_were_sequence_found] = test_parameters;
+    SECTION(test_description)
     {
-        REQUIRE_THAT(jungles::sequence_finder{"makapaka"}, InString("makapaka").FindsSequenceAtIndex(7));
-    }
-    SECTION("Single sequence in the middle")
-    {
-        REQUIRE_THAT(jungles::sequence_finder{"maka"}, InString("dasdasdasmaka").FindsSequenceAtIndex(12));
-    }
-    SECTION("Three sequences in a row")
-    {
-        REQUIRE_THAT(jungles::sequence_finder{"uga"}, InString("ugaugauga").FindsSequenceAtIndices({2, 5, 8}));
-    }
-    SECTION("A sequence which contains string that is repeated in the sequence and this string is repeated 4 times so "
-            "it's only found two times")
-    {
-        REQUIRE_THAT(jungles::sequence_finder{"ugauga"}, InString("ugaugaugauga").FindsSequenceAtIndices({5, 11}));
+        for (auto index : indices_were_sequence_found)
+        {
+            if (index >= input_string.size())
+            {
+                FAIL("Test preparation failure: provided index: "
+                     << index << " is out of bound for input string with size: " << input_string.size());
+            }
+        }
+
+        jungles::sequence_finder sf{sequence_to_find};
+        for (unsigned i = 0; i < input_string.size(); ++i)
+        {
+            const auto &indices = indices_were_sequence_found;
+
+            auto shall_be_found{std::find(std::begin(indices), std::end(indices), i) != std::end(indices)};
+            auto found = sf.found(input_string[i]);
+
+            if ((shall_be_found && !found) || (!shall_be_found && found))
+            {
+                FAIL(make_test_failure_message(shall_be_found, found, i));
+            }
+        }
     }
 }
+
+static std::string make_test_failure_message(bool shall_be_found, bool found, unsigned index)
+{
+    std::ostringstream ss;
+    ss << "At index " << index;
+    ss << ", sequence shall" << (shall_be_found ? " " : " NOT ") << "be found, ";
+    ss << "but" << (found ? " " : " NOT ") << "found";
+    return ss.str();
+};
